d56a7d9f953ed1bd823afe267f2b5ee5e35848b7
igor <myzzon84@gmail.com>
2024-09-03 23:57:41 +0300

    commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:8000/';
-export const BASE_URL = 'mern-notes-app-backend.vercel.app';
+export const BASE_URL = 'https://mern-notes-app-backend-myzzon84.vercel.app';
